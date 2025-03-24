701c4988798f295ea821a033a56410dbee47bb97
Philip Cole <phil@marketingradar.com>
2025-03-24 18:35:07 +0000

    first full commit

---
+++ index.js
@@ -0,0 +1,29 @@
+document.write('<div class="top-menu-container">\'<li>\<a href="./index.html">HOME</a>\</li>\
+        <li>\<a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\</li>\
+        <li>\<a href="./north-york-moors.html">NORTH YORK MOORS</a>\</li>\
+        <li>\<a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\</li>\
+        <li>\<a href="./lake-district.html">LAKE DISTRICT</a>\</li>\
+        <li>\<a href="./peak-district.html">PEAK DISTRICT</a>\</li>\
+        <li>\<a href="./wales.html">WALES</a>\</li>\
+        <li>\<a href="./cornwall.html">CORNWALL</a>\</li>\
+        <li>\<a href="./northumberland.html">NORTHUMBERLAND</a>\</li>\
+        <li>\<a href="./durham_2021_05_hurworth_burn_reservoir.html">DURHAM</a>\</li>\
+        <li>\<a href="./teesdale_2018_09_high_force.html">TEESDALE</a>\</li>\
+        <li>\""\</li>\</div>');
+
+document.write('<div class="navbar">\
+    <div class="dropdown">\
+    <button class="dropbtn" onclick="myFunction()">TOURS<i class="fa fa-caret-down"></i></button>\
+    <div class="dropdown-content" id="myDropdown">\
+    <a href="./index.html">HOME</a>\
+    <a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\
+    <a href="./north-york-moors.html">NORTH YORK MOORS</a>\
+    <a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\
+    <a href="./lake-district.html">LAKE DISTRICT</a>\
+    <a href="./wales.html">WALES</a>\
+    <a href="./cornwall.html">CORNWALL</a>\
+    <a href="./northumberland.html">NORTHUMBERLAND</a>\
+    <a href="./durham_2021_05_hurworth_burn_reservoir.html">DURHAM</a>\
+    <a href="./teesdale_2018_09_high_force.html">TEESDALE</a>\
+    </div></div></div>');
+    
