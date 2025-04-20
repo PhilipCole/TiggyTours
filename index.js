99562461e1449172d449dc5389b15bc7548b4cce
PhilipCole <phil@marketingradar.com>
2025-04-20 21:04:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.write('<div class="top-menu-container">\'\
         <li>\<a href="./search_screen.html"><i class="fa fa-search"></i></a>\</li>\
         <li>\<a href="./index.html">HOME</a>\</li>\
-        <li>\<a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\</li>\
-        <li>\<a href="./north-york-moors.html">NORTH YORK MOORS</a>\</li>\
-        <li>\<a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\</li>\
-        <li>\<a href="./lake-district.html">LAKE DISTRICT</a>\</li>\
-        <li>\<a href="./peak-district.html">PEAK DISTRICT</a>\</li>\
-        <li>\<a href="./wales.html">WALES</a>\</li>\
         <li>\<a href="./cornwall.html">CORNWALL</a>\</li>\
-        <li>\<a href="./northumberland.html">NORTHUMBERLAND</a>\</li>\
         <li>\<a href="./durham.html">DURHAM</a>\</li>\
-        <li>\<a href="./teesside_2025_04_eston_nab.html">TEESSIDE</a>\</li>\
+        <li>\<a href="./lake-district.html">LAKE DISTRICT</a>\</li>\
+        <li>\<a href="./north-york-moors.html">NORTH YORK MOORS</a>\</li>\
+        <li>\<a href="./northumberland.html">NORTHUMBERLAND</a>\</li>\
+        <li>\<a href="./peak-district.html">PEAK DISTRICT</a>\</li>\
+        <li>\<a href="./teesside.html">TEESSIDE</a>\</li>\
+        <li>\<a href="./wales.html">WALES</a>\</li>\
+        <li>\<a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\</li>\
+        <li>\<a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\</li>\
         <li>\""\</li>\</div>');
 
 document.write('<div class="navbar">\
@@ -19,30 +19,31 @@ document.write('<div class="navbar">\
     <button class="dropbtn" onclick="myFunction()">TOURS <i class="fa fa-caret-down"></i></button>\
     <div class="dropdown-content" id="myDropdown">\
     <a href="./index.html">HOME</a>\
-    <a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\
-    <a href="./north-york-moors.html">NORTH YORK MOORS</a>\
-    <a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\
-    <a href="./lake-district.html">LAKE DISTRICT</a>\
-    <a href="./wales.html">WALES</a>\
     <a href="./cornwall.html">CORNWALL</a>\
-    <a href="./northumberland.html">NORTHUMBERLAND</a>\
     <a href="./durham.html">DURHAM</a>\
-    <a href="./teesside_2025_04_eston_nab.html">TEESSIDE</a>\
+    <a href="./lake-district.html">LAKE DISTRICT</a>\
+    <a href="./north-york-moors.html">NORTH YORK MOORS</a>\
+    <a href="./northumberland.html">NORTHUMBERLAND</a>\
+    <a href="./teesside.html">TEESSIDE</a>\
+    <a href="./wales.html">WALES</a>\
+    <a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\
+    <a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\
     </div></div></div>');
 
     document.write('<div class="navbar-laptop">\
         <li>\<a href="./search_screen.html"><i class="fa fa-search"></i></a>\</li>\
         <a href="./index.html">HOME</a>\
-        <a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\
-        <a href="./north-york-moors.html">NORTH YORK MOORS</a>\
-        <a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\
         <a href="./lake-district.html">LAKE DISTRICT</a>\
-        <a href="./wales.html">WALES</a>\
+        <a href="./north-york-moors.html">NORTH YORK MOORS</a>\
         <div class="dropdown-laptop">\
         <button class="dropbtn-laptop" onclick="myFunction2()">OTHER <i class="fa fa-caret-down"></i></button>\
         <div class="dropdown-laptop-content" id="myDropdown2">\
         <a href="./cornwall.html">CORNWALL</a>\
-        <a href="./northumberland.html">NORTHUMBERLAND</a>\
         <a href="./durham.html">DURHAM</a>\
-        <a href="./teesside_2025_04_eston_nab.html">TEESSIDE</a>\
-        </div></div></div>');
+        <a href="./northumberland.html">NORTHUMBERLAND</a>\
+        <a href="./teesside.html">TEESSIDE</a>\
+        </div></div>\
+        <a href="./wales.html">WALES</a>\
+        <a href="./yorkshire-coast.html">YORKSHIRE COAST</a>\
+        <a href="./yorkshire-dales.html">YORKSHIRE DALES</a>\
+        </div>');
